8d235a58a6cc3a3eec994c157789aa2860d55495
Gessatori <salvatorechiaramonte5969@gmail.com>
2026-06-13 13:50:15 +0200

    Correggi chiave Supabase e vista materiale admin

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Non inserire mai qui la service_role/secret key.
 window.TECNOPLAFON_CONFIG = {
   SUPABASE_URL: "https://rmsrhtqtmcmgotgiediu.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6InJtc3JodHF0bWNtZ290Z2llZGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MzE0OTIsImV4cCI6MjA5NjQwNzQ5Mn0.BI7C7p84cRyk83ZcKXKVPVcN5hTtbeq8z-jY8m45VA8"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtc3JodHF0bWNtZ290Z2llZGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MzE0OTIsImV4cCI6MjA5NjQwNzQ5Mn0.BI7C7p84cRyk83ZcKXKVPVcN5hTtbeq8z-jY8m45VA8"
 };
 
 // Miglioramento schermata Admin > Richieste materiale.
